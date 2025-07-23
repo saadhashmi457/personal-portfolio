9131f1301528cdf330cb54fc5883cd5dd64a3369
saadhashmi457 <hashmisaad487@gmail.com>
2025-07-23 15:58:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Software Engineer", "Web Developer","Master of python"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop:true
+})
